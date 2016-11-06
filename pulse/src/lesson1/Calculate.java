8d9a259d3664db5d7ec70c466a815fde223cddfa
Ruslan <zhd.ruslan@gmail.com>
2016-11-06 14:41:28 +0200

    small changes

--- pulse/src/lesson1/Calculate.java
+++ pulse/src/lesson1/Calculate.java
@@ -5,20 +5,20 @@ package lesson1;
  */
 public class Calculate {
 
-    public static void doOperation(int firstOper, int secOper, String operation) {
+    public static void doOperation(int param1, int param2, String operation) {
 
         switch (operation) {
             case "+":
-                System.out.println(firstOper + secOper);
+                System.out.println(param1 + param2);
                 break;
             case "-":
-                System.out.println(firstOper - secOper);
+                System.out.println(param1 - param2);
                 break;
             case "*":
-                System.out.println(firstOper * secOper);
+                System.out.println(param1 * param2);
                 break;
             case "/":
-                System.out.println(firstOper / secOper);
+                System.out.println(param1 / param2);
                 break;
             default:
                 System.out.println("Wrong input!");
--- pulse/src/lesson1/Main.java
+++ pulse/src/lesson1/Main.java
@@ -1,8 +1,34 @@
 package lesson1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int arg1 = 0;
+        int arg2 = 0;
+
+        while (true){
+
+            try {
+                arg1 = scanner.nextInt();
+            } catch (InputMismatchException exc){
+                System.out.println("Wrong value! Try again.");
+                continue;
+            }
+
+            try {
+                arg2 = scanner.nextInt();
+            } catch (InputMismatchException exc){
+                System.out.println("Wrong value! Try again.");
+                continue;
+            }
+
+            String operation = scanner.next();
+
+            Calculate.doOperation(arg1, arg2, operation);
+        }
     }
 }
