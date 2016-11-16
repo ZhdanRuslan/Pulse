f38057aa284da9fafa168db50d12393360fd737e
Ruslan <zhd.ruslan@gmail.com>
2016-11-16 18:30:26 +0200

    init new app

---
+++ pulse/src/main/java/lesson4/task1/CrazyWorker.java
@@ -0,0 +1,7 @@
+package lesson4.task1;
+
+/**
+ * Created by admin on 16.11.2016.
+ */
+public class CrazyWorker {
+}
---
+++ pulse/src/main/java/lesson4/task1/Employee.java
@@ -0,0 +1,8 @@
+package lesson4.task1;
+
+/**
+ * Created by admin on 16.11.2016.
+ */
+public interface Employee {
+    void addDetail(int amount);
+}
